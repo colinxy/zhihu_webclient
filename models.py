abd3b061e786900d21a8726b5f44a57853d8a6e5
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-07-13 17:13:56 -0700

    basic functionality

--- models.py
+++ models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    topic_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "<Topic: {}>".format(self.name)
+
+
+class People(models.Model):
+    name = models.CharField(max_length=200)
+    handle = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "<People: {}>".format(self.name)
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=200)
+    question_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "<Question: {}>".format(self.name)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    answer_id = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "<Answer: {}, Question: {}>".\
+            format(self.name, self.question.question_id)
--- views.py
+++ views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+import requests
+from fake_useragent import UserAgent
+
+BASE_URL = "http://www.zhihu.com/"
+user_agent = {"User-Agent": UserAgent().random}
+
+
+def index(request):
+    return HttpResponse("<h1>Welcome to Zhihu Client</h1>")
+
+
+def question(request, question_id):
+    url = BASE_URL + "question/" + question_id
+    req = requests.get(url, headers=user_agent)
+
+    if req.status_code != requests.codes.ok:
+        print(url)
+        return HttpResponse("not found")
+
+    return HttpResponse(req.text)
+
+
+def answer(request, question_id, answer_id):
+    url = BASE_URL + "question/" + question_id + "/answer/" + answer_id
+    req = requests.get(url, headers=user_agent)
+
+    if req.status_code != requests.codes.ok:
+        print(url)
+        return HttpResponse("not found")
+
+    return HttpResponse(req.text)
+
+
+def people(request, handle):
+    url = BASE_URL + "people/" + handle
+    req = requests.get(url, headers=user_agent)
+
+    if req.status_code != requests.codes.ok:
+        print(url)
+        return HttpResponse("not found")
+
+    return HttpResponse(req.text)
